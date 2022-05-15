9ce3413fe74100ac98da7d092ac2b65754e009c2
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-05-15 18:47:21 -0400

    modify answers

--- scriptQuestions.js
+++ scriptQuestions.js
@@ -1,58 +1,111 @@
 var questions = [
     {   
-        question: "How does a WHILE loop start?",
-        selection: ["while i = 1 to 10", "while (i <= 10; i++)", "while (var i = 0; i = 10; i++)", "while (i <= 10)"],
-        answer: "while (i <= 10)"
+        question: "What is Katrina's Favorite Bread?",
+        selection: [
+        "Croissant",
+        "Sourdough",
+        "Pineapple Bread",
+        "Pita"
+        ],
+        answer: "Croissant"
     },
     {   
-        question: "The external JavaScript file must contain the <script> tag.",
-        selection: ["True", "False", "Maybe", "I don't know"],
-        answer: "False"
+        question: "What is Kat's Favorite Clothing Brand as of May 2022?",
+        selection: [
+        "COS",
+        "Theory",
+        "Oak+Fort",
+        "Uniqlo"
+       ],
+        answer: "Theory"
     },
     {   
-        question: "Inside which HTML element do we put the JavaScript?", 
-        selection: ["<script>", "<js>", "<javascript>", "<JS>"],
-        answer: "<script>"
+        question: "What instrument does Kat play (well lol)?", 
+        selection: [
+        "piano",
+        "violin",
+        "recorder",
+        "flute"
+        ],
+        answer: "flute"
     },
     {
-        question: "How to write an IF statement in JavaScript?",
-        selection: ["if i = 5", "if (i == 5)", "if i = 5 then", "if i == 5 then"],
-        answer: "if (i == 5) "
+        question: "What Is Kat's Favorite Music Genre?",
+        selection: [
+        "Kpop",
+        "Jpop",
+        "Metal",
+        "Indie"],
+        answer: "Jpop"
     },
     {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        selection: ["<script name='./xxx.js'>", "<script href='./xxx.js'>", "<script scr='./xxx.js'>", "<script ='./xxx.js'>"],
-        answer: "<script scr='./xxx.js'>"
+        question: "What is Kat's go-to Boba Order?",
+        selection: [ 
+        "Classic Bubble Tea",
+        "Brown Sugar Milk",
+        "Strawberry Green Tea with Lychee Jelly",
+        "Yakult Green Tea"
+        ],
+        answer: "Yakult Green Tea"
     },
     {
-        question: "How can you add a comment in a JavaScript?",
-        selection: ["'This is a comment", "//This is a comment", "<!--This is a comment-->", "<!**This is a comment**>"],
-        answer: "//This is a comment"
+        question: "What is Kat's go-to restaurant in Chinatown NYC?",
+        selection: [
+        "Go Go Dim Sum",
+        "The Original Bodai Restaurant",
+        "Spring Cafe on Canal Street",
+        "Happy Veggie Restaurant"
+        ],
+        answer: "Spring Cafe on Canal Street"
     },
     {
-        question: "How do you find the number with the highest value of x and y?",
-        selection: ["Math.max(x, y)", "ceil(x, y)", "top(x, y)", "Math.ceil(x, y) "],
-        answer: "Math.max(x, y)"
+        question: "Kat's favorite dish?",
+        selection: [
+        "Pasta (ANY TYPE)", 
+        "Ramen", 
+        "Mandy's Salad with Sesame Dressing", 
+        "All Of The Above"
+        ],
+        answer: "All Of The Above"
     },
     {
-        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
-        selection: ["if (i != 5)", "if (i <> 5)", "if i =! 5 then", "if i <> 5"],
-        answer: "if (i != 5)"
+        question: "Kat's Favorite Ghibli Animation Film",
+        selection: [
+        "Spirited Away", 
+        "Ponyo", 
+        "Howl's Moving Castle", 
+        "Castle in the Sky"],
+        answer: "Howl's Moving Castle"
     },
     {
-        question: "How do you create a function in JavaScript?",
-        selection: ["function = myFunction()", "function:myFunction()", "function myFunction()", "$Function()"],
-        answer: "function myFunction()"
+        question: "How Many Languages Does Kat Know Fluently",
+        selection: [
+        "3", 
+        "4", 
+        "5", 
+        "2"
+        ],
+        answer: "4"
     },
     {
-        question: "How do you round the number 7.25, to the nearest integer?",
-        selection: ["round(7.25)", "Math.rnd(7.25)", "Math.round(7.25)", "rnd(7.25)"],
-        answer: "Math.round(7.25)"
+        question: "What is Kat's Chinese Zodiac Sign",
+        selection: [
+        "Rabbit", 
+        "Tiger", 
+        "Ox", 
+        "Dragon"
+        ],
+        answer: "Tiger"
     },
     {
-        question: "JavaScript is the same as Java?",
-        selection: ["True", "False", "Maybe", "I don't know"],
-        answer: "False"
+        question: "What is Kat's Spirit Animal",
+        selection: [
+        "Dolphin", 
+        "Cheeta", 
+        "Koala", 
+        "Blobfish"
+        ],
+        answer: "Koala"
     }
 
 ];
