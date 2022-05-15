01f831dc3eec748f3d457cd8bf093048c9d8cffb
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-05-15 16:13:12 -0400

    changing script

---
+++ scriptQuestions.js
@@ -0,0 +1,67 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is Katrina's Favorite Bread?",
+    answer: "Croissant",
+    options: [
+      "Croissant",
+      "Sourdough",
+      "Pineapple Bread"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is Kat's Favorite Clothing Brand as of May 2022?",
+    answer: "Theory",
+    options: [
+      "COS",
+      "Theory",
+      "Oak+Fort"
+    ]
+  },
+    {
+    numb: 3,
+    question: "WWhat instrument does Kat play (well lol)?",
+    answer: "flute",
+    options: [
+      "piano",
+      "violin",
+      "flute"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What Is Kat's Favorite Music Genre?",
+    answer: "Jpop",
+    options: [
+      "Kpop",
+      "Jpop",
+      "Metal",
+      "Indie"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is Kat's go-to Boba Order?",
+    answer: "Yakult Green Tea",
+    options: [
+        "Classic Bubble Tea",
+        "Brown Sugar Milk",
+        "Strawberry Green Tea with Lychee Jelly",
+        "Yakult Green Tea"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is Kat's go-to restaurant in Chinatown NYC?",
+    answer: "Spring Cafe on Canal Street",
+    options: [
+      "Go Go Dim Sum",
+      "The Original Bodai Restaurant",
+      "Spring Cafe on Canal Street",
+      "Happy Veggie Restaurant"
+    ]
+  },
+];
+
